a894b18f784ecda42d9979077b16055811b3b232
Alek Gartland <agartlan@ggc.edu>
2017-04-23 18:58:45 -0400

    monster table

---
+++ monster.sql
@@ -0,0 +1,10 @@
+INSERT INTO `monsters` (`monster_id`, `monster_name`, `description`, `img`, `cmd`, `hp`, `attack`, `item`) VALUES
+('mn_01', 'Crusty Box', 'It looks like a regular cardboard box painted like a treasure chest. In fact, it’s really a monster disguised as a cardboard box painted like a treasure chest. Watch out for its lid! Don’t get a papercut!', '', '', 10, 2, '');
+('mn_02', 'Swarm of Bats', 'Anyone else love fighting bats in video games? Me neither.', '', '', 15, 4, '');
+('mn_03', 'Skeleton', 'A skeleton who knew that, in order to be big and strong when it grew up, it needed to drink plenty of milk. All that calcium has finally paid off!', '', '', 15, 4, '');
+('mn_04', 'Boogeyman', 'One thing is for sure: he is not coming from your closet or from under your bed. To face your fears, you must not hide. Enjoys playing Cheese & Crackers.', '', '', 20, 6, 'it_14');
+('mn_05', 'Zombie', 'That boy Carl just does not listen, does he?', '', '', 20, 6, '');
+('mn_06', 'Captain Backbeard', 'Don’t let his clean-shaven face fool you. He has grown out the hair on his back and braided it! So majestic. It makes you weep in envy and cringe in horror. Shave and a haircut: two bits walk the plank!', '', '', 25, 8, 'it_16');
+('mn_07', 'Floating Bedsheet', 'A poltergeist that thinks it is a ghost.', '', '', 25, 8, 'it_24');
+('mn_08', 'Frankenstein', 'He is the pedantic type who likes to correct people when they get his name wrong.', '', '', 35, 9, '');
+('mn_09', 'Count Dracula', 'this is a description of the first room', '', '', 100, 30, 'it_07');
